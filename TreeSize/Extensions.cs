6a5708c9aa2c0cef774dbba2d7ea0cd65d0a2d46
Максим Шемургалкин <m.shemur@gmail.com>
2019-10-03 21:59:24 +0300

    Renamed extension class

--- TreeSize/Extensions.cs
+++ TreeSize/Extensions.cs
@@ -6,16 +6,18 @@ using System.Threading.Tasks;
 
 namespace TreeSize
 {
-   public static class ExtensionMethod
+   public static class Extensions
     {
         public static double BytesToKilobytes(this double bytes)
         {
             return bytes / 1024d;
         }
+
         public static double BytesToMegabytes(this double bytes)
         {
             return bytes / 1024d / 1024d;
         }
+
         public static double BytesToGigabytes(this double bytes)
         {
             return bytes / 1024d / 1024d/1024d;
